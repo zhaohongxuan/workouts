b441686a4504bf37c9842d47430c815fac2ceaf8
zhaohongxuan <hxzhenu@gmail.com>
2026-05-16 21:06:13 +0800

    chore: remove unless code;

--- assets/index.tsx
+++
@@ -1,2 +0,0 @@
-export const yearStats = import.meta.glob('./year_*.svg', { import: 'ReactComponent' })
-export const totalStat = import.meta.glob(['./github.svg', './grid.svg'], { import: 'ReactComponent' })
