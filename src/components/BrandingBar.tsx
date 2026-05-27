c7a4fc2d4cb34e434312e42fe2e1c77d0b695e34
zhaohongxuan <hxzhenu@gmail.com>
2026-05-27 08:00:13 +0800

    feat: add className prop to BrandingBar component

--- src/components/BrandingBar.tsx
+++ src/components/BrandingBar.tsx
@@ -1,8 +1,12 @@
 import siteMetadata from '../static/site-metadata'
 
-export function BrandingBar() {
+interface BrandingBarProps {
+  className?: string
+}
+
+export function BrandingBar({ className = '' }: BrandingBarProps) {
   return (
-    <div className="flex items-center gap-2">
+    <div className={`flex items-center gap-2 ${className}`}>
       <img src={siteMetadata.logo} alt="avatar" className="w-7 h-7 rounded-full" />
       <div className="flex flex-col gap-0.5">
         <span className="text-xs font-semibold">WORKOUT LOG</span>
